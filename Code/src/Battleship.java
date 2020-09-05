e180b71eeee6c6b26ff3ed6a1855b5cf471dd0f6
Sean Cunningham <47498393+SeanCBlue652@users.noreply.github.com>
2020-09-05 17:43:46 -0500

    Added helpful comments / Fixed the hello world code

--- Code/src/Battleship.java
+++ Code/src/Battleship.java
@@ -1,9 +1,9 @@
-
+//If classes come from files in the same folder, you don't have to import them :)
 
 public class Battleship {
     public static void main(String[] args) {
         //This the the program's main
-        executive exec = new executive;
-        System.out.println(exec.helloWorld());
+        executive exec = new executive(); // This is me instantiating an executive class, which comes from the other file in the same folder.
+        System.out.println(exec.helloWorld()); //You can compile this program in the terminal with "javac Battleship.java" and run it with "java Battleship"
     }
 }
