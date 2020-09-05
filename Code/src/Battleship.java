f51166f0a7786e3482d96d51a1643515804ba87d
Sean Cunningham <47498393+SeanCBlue652@users.noreply.github.com>
2020-09-05 17:35:30 -0500

    Added a gitignore

--- Code/src/Battleship.java
+++ Code/src/Battleship.java
@@ -1,6 +1,9 @@
+
+
 public class Battleship {
-    public static void public static void main(String[] args) {
+    public static void main(String[] args) {
         //This the the program's main
-        System.out.println("Hello world!");
+        executive exec = new executive;
+        System.out.println(exec.helloWorld());
     }
 }
--- Code/src/executive.java
+++ Code/src/executive.java
@@ -1,3 +1,5 @@
 public class executive {
-    
+    public String helloWorld() {
+        return("Hello world!");
+    }
 }
