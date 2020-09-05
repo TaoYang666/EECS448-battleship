b6e9a1e99363c0a8e3c55c26879891af41c32f0e
Sean Cunningham <47498393+SeanCBlue652@users.noreply.github.com>
2020-09-05 17:01:41 -0500

    Initial repo structuring
    Deleted the example program, outlined basic structure of the repository

---
+++ Code/src/Battleship.java
@@ -0,0 +1,6 @@
+public class Battleship {
+    public static void public static void main(String[] args) {
+        //This the the program's main
+        System.out.println("Hello world!");
+    }
+}
