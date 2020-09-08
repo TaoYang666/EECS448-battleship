619b94fa0fe5032ca16fb63c9218f8760d1ca714
Tso, Peter <tsopeter@ku.edu>
2020-09-08 16:59:23 -0500

    Added CollisionHandler
    This simple class handles detection of collisions inside the board with arbitrary characters and out of bounds detection.
    
    Examples of when to use CollisionHandler:
    
    When inputing a ship onto a board, call CollisionHandler on each spot the ship might occupy before altering the board. CollisionHandler will return true if it detects either a collision with another ship (that has the same character) and it reaches out of bounds of the board.
    
    That's it!

---
+++ Code/src/CollisionHandler.java
@@ -0,0 +1,46 @@
+/**
+*	@Name:	 Peter Tso
+*	@KUID:	 2936825
+*	@Email:  tsopeter@ku.edu
+*	@Brief:
+*		This is the collision handler file. When given char marker, it
+*		checks the given Board Object at the position (x,y).
+*		Then returns true if there is a collision, or
+*		false when there is no collision.
+*	@Note:
+*		There are no protections from OutOfBounds. Please
+*		implement protections on your own code when using
+*		CollisionHandler.java
+*
+*		Update:	20200908
+*
+*		Added out of bounds detection.
+*
+*	@Req'd:
+*		The required files are below:
+*
+*		--Board.java.
+*
+*		Thank you.
+*
+*	@Date_Created:					09-05-2020
+*/
+public class CollisionHandler {
+
+	/**
+	*	@pre:	 must be given a constructed Board and valid (x,y) position
+	*	@post:	 checks to see if given place is same to marker
+	*	@return: boolean
+	*	@note:
+	*		When using check(), please pass by value of Board
+	*/
+	public static boolean check(Board g, char marker, int x, int y){
+		if(x < 0 || y < 0 || x >= g.getXSize() || y >= g.getYSize()){
+			return true;
+		}
+		if(g.getMarker(x, y) == marker){
+			return true;
+		}
+		return false;
+	}
+}
